2958cacc4b9116da4c7cd1299d8693ff959eb1e2
Ksinny <102038572+Ksinny@users.noreply.github.com>
2024-05-09 21:01:01 +0900

    Title: 알파벳에 따른 평가 2, Time: 102ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240509/알파벳에 따른 평가 2/evaluation-by-alphabet-2.py
@@ -0,0 +1,14 @@
+c = input()
+
+if c == 'S':
+    print("Superior")
+elif c == 'F':
+    print("Fantastic")
+elif c == 'G':
+    print("Good")
+elif c == "U":
+    print("Usually")
+elif c == "E":
+    print("Effort")
+else:
+    print("Failure")
