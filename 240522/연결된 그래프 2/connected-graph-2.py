fec8d8625bd02081c005c989333a0d28f1dd2604
Ksinny <102038572+Ksinny@users.noreply.github.com>
2024-05-22 18:43:03 +0900

    Title: 연결된 그래프 2, Time: 802ms, Memory: 46MB, Status: Wrong Answer - CodeTree

---
+++ 240522/연결된 그래프 2/connected-graph-2.py
@@ -0,0 +1,29 @@
+count = 0
+answer = 0
+
+def DFS(v):
+    global count
+    ch[v] += 1
+    for nv in graph[v]:
+        if ch[nv] == 0:
+            count += 1
+            DFS(nv)
+
+
+n, m = map(int, input().split())
+graph = [[] for _ in range(n + 1)]
+ch = [0] * (n + 1)
+
+for i in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+
+for i in range(1, n+1):
+    count = 0
+    if ch[i] == 0:
+        DFS(i)
+        ch = [0] * (n + 1)
+
+    if answer <= count:
+        answer = count
+        print(i, end=' ')
