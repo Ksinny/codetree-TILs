e00d045427785da10dedbe45e832f49f9f6b1418
Ksinny <102038572+Ksinny@users.noreply.github.com>
2024-05-22 19:01:43 +0900

    Title: 연결된 그래프 2, Time: 1186ms, Memory: 101MB, Status: Passed - CodeTree

--- 240522/연결된 그래프 2/connected-graph-2.py
+++ 240522/연결된 그래프 2/connected-graph-2.py
@@ -1,5 +1,4 @@
 count = 0
-answer = 0
 
 def DFS(v):
     global count
@@ -13,6 +12,7 @@ def DFS(v):
 n, m = map(int, input().split())
 graph = [[] for _ in range(n + 1)]
 ch = [0] * (n + 1)
+arr = [0] * (n + 1)
 
 for i in range(m):
     a, b = map(int, input().split())
@@ -23,7 +23,9 @@ for i in range(1, n+1):
     if ch[i] == 0:
         DFS(i)
         ch = [0] * (n + 1)
+    arr[i] = count
 
-    if answer <= count:
-        answer = count
+
+for i in range(len(arr)):
+    if arr[i] == max(arr):
         print(i, end=' ')
