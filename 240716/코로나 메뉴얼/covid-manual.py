5a8558dcf72c3a3b287f12eaad0db50a1a747f42
Ksinny <102038572+Ksinny@users.noreply.github.com>
2024-07-16 17:54:40 +0900

    Title: 코로나 메뉴얼, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240716/코로나 메뉴얼/covid-manual.py
@@ -0,0 +1,20 @@
+a_cold, a_temp = input().split()
+b_cold, b_temp = input().split()
+c_cold, c_temp = input().split()
+a_temp, b_temp, c_temp = int(a_temp), int(b_temp), int(c_temp)
+
+cnt = 0
+
+if a_cold=="Y" and a_temp>=37:
+    cnt += 1
+
+if b_cold=="Y" and b_temp>=37:
+    cnt += 1
+
+if a_cold=="Y" and a_temp>=37:
+    cnt += 1
+
+if cnt>=2:
+    print("E")
+else:
+    print("N")
