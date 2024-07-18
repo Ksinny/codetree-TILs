be50c4c509fec8949cd14709c353aabcc0811074
Ksinny <102038572+Ksinny@users.noreply.github.com>
2024-07-18 18:49:01 +0900

    Title: 369 게임, Time: 100ms, Memory: 24MB, Status: Passed - CodeTree

--- 240718/369 게임/369-game.py
+++ 240718/369 게임/369-game.py
@@ -1,7 +1,11 @@
 n = int(input())
 
 for i in range(1, n+1):
-    if i%3==0 or i - (i//10)*10 == 3 or i - (i//10)*10== 6 or i - (i//10)*10 == 9:
+    # 3의 배수인 경우
+    if i%3==0: 
+        print(0, end=' ')
+    # 3, 6, 9가 들어가는 경우
+    elif i//10==3 or i//10==6 or i//10==9 or i%10==3 or i%10==6 or i%10==9:
         print(0, end=' ')
     else:
         print(i, end=' ')
