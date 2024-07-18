a09d2ffa1987265949154dd0081c5033ca3e2c7e
Ksinny <102038572+Ksinny@users.noreply.github.com>
2024-07-18 18:28:31 +0900

    Title: a 부터 b 까지, Time: 100ms, Memory: 24MB, Status: Passed - CodeTree

--- 240718/a 부터 b 까지/a-to-b.py
+++ 240718/a 부터 b 까지/a-to-b.py
@@ -1,13 +1,9 @@
 a, b = map(int, input().split())
 
-
-# break, continue 사용X
 for i in range(a, b+1):
-    if a*2<=b or a+3<=b:
+    if a<=b:
         print(a, end=' ')
         if i%2==1:
             a *= 2
         elif i%2==0:
             a += 3
-
-print(a)
