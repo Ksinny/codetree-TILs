36e87f2eed4f15118c60e5154452e1c0b957fda0
Ksinny <102038572+Ksinny@users.noreply.github.com>
2024-07-18 17:29:54 +0900

    Title: a/b 출력, Time: 132ms, Memory: 73MB, Status: Passed - CodeTree

--- 240718/a/b 출력/a-divide-b.py
+++ 240718/a/b 출력/a-divide-b.py
@@ -1,10 +1,12 @@
 a, b = map(int, input().split())
 
+
+# print(f"{a//b}.", end='')
 for i in range(21):
-    k = a//b
-    s = a%b
-    a = s*10
-    print(k, end='')
-    
-    if i == 0:
+    quot = a//b
+    remain = a%b
+    a = remain*10
+    print(quot, end='')
+
+    if i==0:
         print(".", end='')
